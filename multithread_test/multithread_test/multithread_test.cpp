651a31fcfe09dd0e4ef79e0cf67b359a0907713c
1051823277 <1051823277@qq.com>
2020-09-29 21:44:10 +0800

    locj_guard

--- multithread_test/multithread_test/multithread_test.cpp
+++ multithread_test/multithread_test/multithread_test.cpp
@@ -2,6 +2,19 @@
 #include <thread>
 #include <time.h>
 #include <mutex>
+#include <atomic>
+
+int increase(int* p, int times, std::mutex& mtx)
+{
+	for (int i = 0; i < times; ++i)
+	{
+		std::lock_guard<std::mutex> guard(mtx);
+		//mtx.lock();
+		++*p;
+		//mtx.unlock();
+	}
+	return 0;
+}
 
 int increase(int* p, int times)
 {
@@ -16,24 +29,33 @@ int main()
 {
 	time_t start = clock();//timer start
 
-	int num = 0;
 
-	std::thread thread1(increase, &num, 10000);
+	//std::atomic_int num = 0;
+	int num = 0;
+	std::mutex mtx;
+
+	std::thread thread1([&]() {
+		increase(&num, 10000, mtx);
+	});
+	//std::thread thread1(increase, &num, 10000);
 	std::thread thread2([&]() {
 		for (int i = 0; i < 10000; ++i)
 		{
+			std::lock_guard<std::mutex> guard(mtx);
+			//mtx.lock();
 			++num;
+			//mtx.unlock();
 		}
 	});
 
 	thread1.join();
 	thread2.join();
 
+	
+
+
+
 	std::cout << num << std::endl;
-
-
-
-
 
 	time_t end = clock();//timer stop
 	std::cout << "Time: " << (end - start) << " ms." << std::endl;
