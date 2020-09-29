5c3c8672a2a7de55a2d383a7cbb5fb3869790a76
1051823277 <1051823277@qq.com>
2020-09-29 17:46:06 +0800

    init

---
+++ multithread_test/multithread_test/multithread_test.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <thread>
+#include <time.h>
+#include <mutex>
+
+int increase(int* p, int times)
+{
+	for (int i = 0; i < times; ++i)
+	{
+		++*p;
+	}
+	return 0;
+}
+
+int main()
+{
+	time_t start = clock();//timer start
+
+	int num = 0;
+
+	std::thread thread1(increase, &num, 10000);
+	std::thread thread2([&]() {
+		for (int i = 0; i < 10000; ++i)
+		{
+			++num;
+		}
+	});
+
+	thread1.join();
+	thread2.join();
+
+	std::cout << num << std::endl;
+
+
+
+
+
+	time_t end = clock();//timer stop
+	std::cout << "Time: " << (end - start) << " ms." << std::endl;
+	system("pause");
+	return 0;
+}
